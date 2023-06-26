b93bce6fb8f3fcb653044eb2e621d54acbde769c
Alexander Levenston <alexlevenston2021@gmail.com>
2023-06-25 22:04:29 -0400

    added commented command to generate example curl request

--- token.sh
+++ token.sh
@@ -4,3 +4,4 @@ read -p 'Enter VAULT_ADDR: ' input_VAULT_ADDR
 VAULT_ADDR=${input_VAULT_ADDR:-$VAULT_ADDR}:8200
 
 vault write auth/jwt/login jwt=$1
+#vault write -output-curl-string auth/jwt/login jwt=$1
